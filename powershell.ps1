833a0d7041d1dfea233eb7dd947ffa58852234d6
yatbio <14935907+yatbio@users.noreply.github.com>
2019-04-26 08:48:48 -0700

    Updates
    See comments section in script.

--- powershell.ps1
+++ powershell.ps1
@@ -12,9 +12,15 @@
 * One thing I never thought to ask you - are all of your domain controllers running Windows Server 2012 or better?
 
 * The CSV provided today has "NewUser" but no CopUserTemplate, DB, or AddressBookPolicy defined fields.  Why wouldn't you want to use the ''Copy user template' on NewUser requests?
-
 #>
+<#Mike in IT Comments 4/26/2019 @ 11:44A ET
+* I updated the code a little bit to use the -join operator on strings instead of attempting the odd concat that was happening. Join is easier to read.
 
+* The issue was the sub-expressions expansion - this turned our strings from things like $user.Firstname to $($user.Firstname) - read more about it here -> https://ss64.com/ps/syntax-operators.html
+
+* I also added $DebugPreference = "Continue" right after the comment blocks so if set then Write-Debug cmdlet will output information onto the screen.
+#>
+$DebugPreference = "Continue"
 function Format-CsvValue {
   [CmdletBinding()]
   param (
@@ -67,27 +73,43 @@ $ErrorActionPreference='stop'
 $LogFolder = "$env:userprofile\desktop\logs"
 
 ForEach($User in $Users) {
+   Write-Debug "First Name (CSV): $($user.Firstname)"
+   Write-Debug "Last Name (CSV): $($user.Lastname)"
+   Write-Debug "Company (CSV): $($user.Company)"
+   Write-Debug "Email/Domain (CSV): $($user.'Email/Domain')"
+   Write-Debug "EndDate (CSV): $($user.EndDate)"
+   #etc.etc.
+
   
   #$FirstName = $User.FirstName.substring(0,1).toupper()+$User.FirstName.substring(1).tolower()
   $FirstName = Format-CsvValue -isTitleCase $true -sValue $User.FirstName #using our new function let's properly format the firstname.
+  Write-Debug "First Name (Script): $FirstName"
   #$LastName  = $User.LastName.substring(0,1).toupper()+$User.LastName.substring(1).tolower()
   $LastName = Format-CsvValue -isTitleCase $true -sValue $User.LastName
+  Write-Debug "Last Name (Script): $LastName"
 
-  $FullName = $User.FirstName + " " + $User.LastName #not using Format-CsvValue here because that was already done for $FirstName and $LastName vars seperately
+  $FullName = -join($($User.FirstName)," ",$($User.LastName)) #not using Format-CsvValue here because that was already done for $FirstName and $LastName vars seperately
+  Write-Debug "Full Name (Script): $FullName"
 
-  $SAM = ($user.FirstName.Substring(0,1) + $user.LastName).ToLower #no need to put this on a new line itself do it all in a single step.
+  $SAM = $(-join (($user.FirstName).Substring(0,1),$user.LastName).ToLower())
+  $SAM = $SAM.ToLower()
+  Write-Debug "SAM (Script): $SAM"
 
   #$SAM=$sam.tolower() #I moved this variable up a couple of lines. I like to keep variables together especially when doing multiple operations on the same variable so then you know what happened later :)
   
-  $dnsroot = '@' + (Get-ADDomain).dnsroot
+  $dnsroot = "@$((Get-ADDomain).dnsroot)"
+  Write-Debug "DNS Root (Script): $dnsroot"
 
   $Password = (ConvertTo-SecureString -AsPlainText 'Cloudcom.1' -Force)
 
-  $UPN = $SAM + "$dnsroot" #why are you quoting "$dnsroot" but not $SAM? Why are you quoting at all? :)
+  $UPN = -join ($SAM,$dnsroot)
+  Write-Debug "UPN (Script): $UPN"
 
   $OU = $user.OU #uncommented and changed to read the "OU" header in the CSV file.
+  Write-Debug "OU (Script): $OU"
 
-  $email = $Sam + "$dnsroot" #same comment as for $UPN
+  $email = -join ($Sam,$dnsroot)
+  Write-Debug "Email (Script): $Email"
   #your CSV file contains headers that you aren't bringing into the script
   #Examples;
   #missing 'Company'
@@ -96,6 +118,10 @@ ForEach($User in $Users) {
 
 Try {
     if (!(get-aduser -Filter {samaccountname -eq "$SAM"})){
+      #your CSV file contains headers that do not exist in your $Parameters hash table and they are as follows;
+      #no 'Company' mapping.
+      #no 'StartDate' mapping.
+      #no 'EndDate' mapping.
       $Parameters = @{
         'SamAccountName'        = $Sam
         'UserPrincipalName'     = $UPN 
@@ -108,10 +134,7 @@ Try {
         'Enabled'               = $true 
         'Path'                  = $OU
         'PasswordNeverExpires'  = $False
-        #your CSV file contains headers that do not exist in your $Parameters hash table and they are as follows;
-        #no 'Company' mapping.
-        #no 'StartDate' mapping.
-        #no 'EndDate' mapping.
+
       } #=>Parameters
 
       <#
@@ -130,12 +153,12 @@ Try {
 
     $oNewUser = New-ADUser @Parameters
 
-    Write-Verbose "[PASS] Created $FullName "
-    $successUsers += $FullName + "," +$SAM
+    Write-Verbose "[PASS] Created $FullName"
+    $successUsers += "$FullName , $SAM"
 } Catch {
   $ErrorMessage = $_.Exception.Message
   $FailedItem = $_.Exception.ItemName
-  Write-Warning "[ERROR]Can't create user [$($FullName)] : $_ . Failed with error $ErrorMessage on failed item $FailedItem"
+  Write-Warning "[ERROR]Can't create user [$($FullName)] : $_"
 
   $failedUsers += $FullName + "," +$SAM + "," +$_
 }#=>catch
